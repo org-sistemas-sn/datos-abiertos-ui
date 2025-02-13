da499e5314d48ef9b27e8da68a6aa9cfa484bf6b
Julian <julianfinelli97@gmail.com>
2025-02-13 11:23:11 -0300

    chore: last changes in animation for home

---
+++ src/pages/EventDetail/EventDetail.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function EventDetail() {
+  return (
+    <div className='w-full mt-24 border border-red-500 h-[100vh]'>
+        
+    </div>
+  )
+}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -108,7 +108,7 @@ export const Home = () => {
       const duration = 2500;
       const stepTime = duration / end;
       const timer = setInterval(() => {
-        start += Math.ceil(end / 45);
+        start += Math.ceil(end / 50);
         if (start >= end) {
           start = end;
           clearInterval(timer);
