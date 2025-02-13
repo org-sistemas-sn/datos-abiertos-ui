7b383df7f40ff27a22f5f92dedf9e35bad5a0b8e
Julian <julianfinelli97@gmail.com>
2025-02-13 11:39:33 -0300

    chore: delete cursor-pointer for CardEventCalendary

--- src/components/Cards/CardEventCalendary/index.jsx
+++ src/components/Cards/CardEventCalendary/index.jsx
@@ -2,7 +2,7 @@
 
 export default function CardEventCalendary({ day, title, bgColor }) {
   return (
-    <div className="flex w-full cursor-pointer font-grotesk h-20 items-center select-none p-4 bg-white border rounded-lg shadow transition-transform duration-500 ease-in-out hover:shadow-lg hover:scale-105 lg:max-w-[400px]">
+    <div className="flex w-full font-grotesk h-20 items-center select-none p-4 bg-white border rounded-lg shadow transition-transform duration-500 ease-in-out hover:shadow-lg hover:scale-105 lg:max-w-[400px]">
       <div
         className={`w-24 h-full flex items-center justify-center rounded-lg ${bgColor}`}
       >
